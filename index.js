a79a29977b7db55f8f85e813d50736283fa8d907
Farid <bouras.farid@outlook.fr>
2022-07-25 13:58:06 +0200

    feat: added ".gitignore", ".env" and deploy the backend on heroku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+//gestion des variables d'environnements
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 app.use(cors());
 
@@ -452,16 +456,12 @@ app.get('/', (req, res) => {
 });
 
 // Heroku va nous fournir une variable process.env.PORT
-// if (process.env.PORT) {
-//   app.listen(process.env.PORT, () => {
-//     console.log("Server started");
-//   });
-// } else {
-//   app.listen(3200, () => {
-//     console.log("Server started");
-//   });
-// }
-
-app.listen(process.env.PORT || 3200, () => {
-  console.log('Server started');
-});
+if (process.env.PORT) {
+  app.listen(process.env.PORT, () => {
+    console.log('Server started');
+  });
+} else {
+  app.listen(3200, () => {
+    console.log('Server started');
+  });
+}
